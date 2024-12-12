937b1dc18edf85e267de8053f2a078482bed132c
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2024-12-12 14:05:24 +0100

    Created message entity

---
+++ backend/src/main/java/com/chatApplication/chatApplication/model/Message.java
@@ -0,0 +1,61 @@
+package com.chatApplication.chatApplication.model;
+
+import com.chatApplication.chatApplication.enumeration.Status;
+import lombok.Data;
+
+
+public class Message {
+    
+    private String senderName;
+
+    private String receiverName;
+
+    private String message;
+
+    private String date;
+
+    private Status status;
+
+    public Message() {
+    }
+
+    public String getSenderName() {
+        return senderName;
+    }
+
+    public void setSenderName(String senderName) {
+        this.senderName = senderName;
+    }
+
+    public String getReceiverName() {
+        return receiverName;
+    }
+
+    public void setReceiverName(String receiverName) {
+        this.receiverName = receiverName;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+}
