24bd68bb08c3fadd4d5017818838ca6029822e9e
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2024-12-13 22:06:25 +0100

    Updated app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,7 @@
+import ChatRoom from "./component/ChatRoom";
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <ChatRoom></ChatRoom>;
 }
 
 export default App;
