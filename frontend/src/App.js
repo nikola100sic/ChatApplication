1368f7f8063e1599062c0ba2106b87e48f9938b5
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2024-12-17 12:24:58 +0100

    Updated App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,25 @@
+import { ToastContainer } from "react-toastify";
 import ChatRoom from "./component/ChatRoom";
 
 function App() {
-  return <ChatRoom></ChatRoom>;
+  return (
+    <>
+      <ChatRoom />
+      <ToastContainer
+        position="top-center"
+        autoClose={2500}
+        hideProgressBar
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover={false}
+        theme="colored"
+      />
+      ;
+    </>
+  );
 }
 
 export default App;
