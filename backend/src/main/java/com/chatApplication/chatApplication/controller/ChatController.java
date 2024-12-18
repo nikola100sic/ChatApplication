7ece176c1f475f7345c0c139a1b0d4dee15e7914
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2024-12-18 23:44:45 +0100

    Updated chat controller

--- backend/src/main/java/com/chatApplication/chatApplication/controller/ChatController.java
+++ backend/src/main/java/com/chatApplication/chatApplication/controller/ChatController.java
@@ -1,6 +1,7 @@
 package com.chatApplication.chatApplication.controller;
 
 import com.chatApplication.chatApplication.model.Message;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
@@ -9,17 +10,17 @@ import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
 @Controller
+@RequiredArgsConstructor
 public class ChatController {
 
-    @Autowired
-    private SimpMessagingTemplate simpMessagingTemplate;
+    private final SimpMessagingTemplate simpMessagingTemplate;
 
     @MessageMapping("/message")//    app/message
     @SendTo("/chatroom/public")
     public Message RecievePublicMessage(@Payload Message message) {
         return message;
     }
-
+    
     @MessageMapping("/private-message")
     public Message RecievePrivateMessage(@Payload Message message) {
         simpMessagingTemplate.convertAndSendToUser(message.getReceiverName(), "/private", message); //  user/Nikola/private
